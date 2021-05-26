aec37a0fbb398da299d50929519f20ce9c2f43ca
Harsh <harshjaiswal960@gmail.com>
2021-05-27 02:04:50 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';           //all files that have react containers
+import ReactDom from 'react-dom';    //hook the element in react is going to be stored
+
+import App from './App';
+
+
+ReactDom.render(<App />, document.getElementById('root'));
